bfcf2461e0399bf6f41660139b612cd66a5aa536
Felipe Titonel <felipe.titonel@gmail.com>
2016-09-05 08:05:24 -0300

    isUpdateMode

--- src/main/java/br/com/angrybits/angrybitsWeb/view/ClienteEditMB.java
+++ src/main/java/br/com/angrybits/angrybitsWeb/view/ClienteEditMB.java
@@ -5,14 +5,13 @@ import javax.inject.Inject;
 import br.com.angrybits.angrybitsCore.business.ClienteBC;
 import br.com.angrybits.angrybitsCore.entity.Cliente;
 import br.gov.frameworkdemoiselle.stereotype.Controller;
-import br.gov.frameworkdemoiselle.annotation.PreviousView;
 import br.gov.frameworkdemoiselle.stereotype.ViewController;
 import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
 
 @ViewController
 @Controller
-@PreviousView("/bookmark_list.xhtml")
+//@PreviousView("/bookmark_list.xhtml")
 public class ClienteEditMB  extends AbstractEditPageBean<Cliente, Long>{
 		
 	private static final long serialVersionUID = 1L;
@@ -31,7 +30,7 @@ public class ClienteEditMB  extends AbstractEditPageBean<Cliente, Long>{
 	@Transactional
 	public String insert() {
 		this.clienteBC.insert(getBean());
-		return getPreviousView();
+		return null;
 	}
 
 	@Override
